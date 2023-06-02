d4f146aca672c1cb4e47b5dac3953081c196cb44
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2023-06-02 08:26:36 +0000

    Bug fix in default user factory

--- src/DefaultUserFactory.php
+++ src/DefaultUserFactory.php
@@ -18,6 +18,7 @@ use Drewlabs\HttpGuard\Exceptions\ServerBadResponseException;
 use Drewlabs\HttpGuard\Traits\AttributesAware;
 use Drewlabs\Contracts\OAuth\HasApiTokens;
 use Drewlabs\Core\Helpers\Arr;
+use Drewlabs\Core\Helpers\Reflector;
 
 class DefaultUserFactory implements UserFactory
 {
@@ -67,7 +68,7 @@ class DefaultUserFactory implements UserFactory
             }
         };
 
-        return \count(array_intersect([AttributesAware::class, \Drewlabs\Support\Traits\AttributesAware::class], drewlabs_class_recusive_uses($object))) > 0 ||
+        return \count(array_intersect([AttributesAware::class, \Drewlabs\Support\Traits\AttributesAware::class], Reflector::usesRecursive($object))) > 0 ||
             (method_exists($object, 'createFromAttributes') && $isStatic($object, 'createFromAttributes'));
     }
 }
